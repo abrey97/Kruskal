d3e1451559ff9b062d602ccde1b88f7136ca10b6
Reyes <abril.reyes20@uabc.edu.mx>
2026-04-27 01:46:56 -0700

    commit final

--- codigo.c
+++ codigo.c
@@ -2,7 +2,7 @@
 
 #include <stdio.h>
 
-#define MAX 200  // máximo de aristas
+#define MAX 300  
 
 struct Arista {
     int origen;
@@ -44,7 +44,7 @@ void kruskal(struct Arista aristas[], int vertices, int cantidad) {
     ordenarAristas(aristas, cantidad);
 
     int pesoTotal = 0;
-    printf("Aristas seleccionadas en el MST:\n");
+    printf("\n\nAristas seleccionadas:\n");
 
     for (int i = 0; i < cantidad; i++) {
         int u = aristas[i].origen;
@@ -52,27 +52,33 @@ void kruskal(struct Arista aristas[], int vertices, int cantidad) {
         int w = aristas[i].peso;
 
         if (encontrar(u) != encontrar(v)) {
-            printf("%d -- %d == %d\n", u, v, w);
+            printf("%d ----- %d = %d\n", u, v, w);
             pesoTotal += w;
             unir(u, v);
         }
     }
 
-    printf("Peso total del MST: %d\n", pesoTotal);
+    printf("Peso total: %d\n", pesoTotal);
 }
 
 int main() {
-    int vertices = 20;   // casas A–T
-    int cantidad = /* número de aristas */;
+    int vertices = 20;   // casas
+    int cantidad = 33; //aristas
 
     struct Arista aristas[MAX] = {
-        // Aquí debes meter las conexiones de tu mapa:
-        // {origen, destino, peso}
-        // Ejemplo:
-        {0, 1, 10}, {0, 2, 6}, {0, 3, 5}, {1, 3, 15}, {2, 3, 4}
-        // ...
+        {0, 1, 100}, {0, 3, 85}, {0, 3, 30}, {1, 2, 100},
+        {1, 4, 40}, {1, 5, 45}, {1, 6, 70}, {2, 6, 55},
+        {2, 7, 60}, {3, 15, 50}, {4, 8, 10}, {5, 9, 55},
+        {6, 10, 15}, {6, 11, 20}, {7, 11, 40}, {7, 13, 85},
+        {7, 14, 70}, {8, 9, 60}, {8, 15, 90}, {9, 10, 80},
+        {9, 16, 45}, {10, 12, 60}, {11, 12, 50}, {12, 18, 2},
+        {13, 14, 120}, {13, 14, 105}, {13, 18, 55}, {13, 19, 60},
+        {14, 19, 195}, {15, 17, 205}, {16, 17, 5}, {17, 19, 230},
+        {18, 19, 25}
     };
-
+    
+    printf("\nAlgoritmo voraces: Kruskal\n");
+    
     kruskal(aristas, vertices, cantidad);
 
     return 0;
